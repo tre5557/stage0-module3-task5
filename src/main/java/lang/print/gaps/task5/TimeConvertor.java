6be14d7a3d3fcc7737017c3eaf1884c4cac0358b
Timur Bashirov <bashirov188@gmail.com>
2024-02-28 13:51:00 +0300

    done

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float hours;
+
+        hours = minutes * 60;
+
+        System.out.println(hours);
     }
 }
